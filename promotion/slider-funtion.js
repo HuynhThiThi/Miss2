a200705ad6d0095a20de9145b25c0af121b4cea1
Huynh_Thi <huynhthivoxu@gmail.com>
2022-07-18 23:01:21 +0700

    dropdow select

--- promotion/slider-funtion.js
+++ promotion/slider-funtion.js
@@ -1,43 +1,43 @@
 
-$( document ).ready(function() {
-    $h_slider_options =  {
-      gallery: true,
-      item: 1,
-      loop:true,
-      slideMargin: 0,
-      thumbItem: 3,
-      galleryMargin: 15,
-     thumbMargin: 20,
-      }; 
-  h_slider = $('#lightSlider').lightSlider($h_slider_options);
-  $selector = '#lightSlider li:not(".clone") a';
-  $selector += ',#lightSliderVertical li:not(".clone") a';
-  $().fancybox({
-    width: 560,  // default 800
-    height: 340, // default 600
-    selector : $selector,
-    backFocus : false, 
-    buttons : [          
-      'close'
-    ],
-    parentEl: '.wrapper',
-    btnTpl: {
-      close:
-      '<button data-fancybox-close class="fancybox-button fancybox-button--close" title="{{CLOSE}}">' +
-      '<div class="icon-close"></div>' +
-      "</button>",
-      arrowLeft:
-      '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">' +
-      '<div class="icon-prev">icon</div>' +
-      "</button>",
+// $( document ).ready(function() {
+//     $h_slider_options =  {
+//       gallery: true,
+//       item: 1,
+//       loop:true,
+//       slideMargin: 0,
+//       thumbItem: 3,
+//       galleryMargin: 15,
+//      thumbMargin: 20,
+//       }; 
+//   h_slider = $('#lightSlider').lightSlider($h_slider_options);
+//   $selector = '#lightSlider li:not(".clone") a';
+//   $selector += ',#lightSliderVertical li:not(".clone") a';
+//   $().fancybox({
+//     width: 560,  // default 800
+//     height: 340, // default 600
+//     selector : $selector,
+//     backFocus : false, 
+//     buttons : [          
+//       'close'
+//     ],
+//     parentEl: '.wrapper',
+//     btnTpl: {
+//       close:
+//       '<button data-fancybox-close class="fancybox-button fancybox-button--close" title="{{CLOSE}}">' +
+//       '<div class="icon-close"></div>' +
+//       "</button>",
+//       arrowLeft:
+//       '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">' +
+//       '<div class="icon-prev">icon</div>' +
+//       "</button>",
 
-    arrowRight:
-      '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
-      '<div class="icon-next">icon</div>' +
-      "</button>",
-    }
-  }); 
-});
+//     arrowRight:
+//       '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
+//       '<div class="icon-next">icon</div>' +
+//       "</button>",
+//     }
+//   }); 
+// });
 // $(function() {
 //   var list = $('.dropdown-list');
 //   var link = $('.js-link');
@@ -66,101 +66,28 @@ $( document ).ready(function() {
 //  h_slider = $('#ocassions-slider').lightSlider(h_slider_options);
 // });
 
-document.addEventListener('DOMContentLoaded', createSelect, false);
-function createSelect() {
-    var select = document.getElementsByTagName('select'),
-      liElement,
-      ulElement,
-      optionValue,
-      iElement,
-      optionText,
-      selectDropdown,
-      elementParentSpan;
-
-      for (var select_i = 0, len = select.length; select_i < len; select_i++) {
-        //console.log('selects init');
-
-      select[select_i].style.display = 'none';
-      wrapElement(document.getElementById(select[select_i].id), document.createElement('div'), select_i, select[select_i].getAttribute('placeholder-text'));
-
-      for (var i = 0; i < select[select_i].options.length; i++) {
-        liElement = document.createElement("li");
-        optionValue = select[select_i].options[i].value;
-        optionText = document.createTextNode(select[select_i].options[i].text);
-        liElement.className = 'select-dropdown__list-item';
-        liElement.setAttribute('data-value', optionValue);
-        liElement.appendChild(optionText);
-        ulElement.appendChild(liElement);
-
-        liElement.addEventListener('click', function () {
-          displyUl(this);
-        }, false);
-      }
+$(function() {
+  
+  $('.dropdown_select').on('click', function() {
+    console.log("sdsd")
+    $(this).toggleClass('open');
+  });
+  
+  $('.dropdown_select > .list > .item').on('click', function() {
+    $('.dropdown_select > .list > .item').removeClass('selected');
+    $(this).addClass('selected').parent().parent().removeClass('open').children('.caption').text( $(this).text() );
+  });
+  
+  $(document).on('keyup', function(evt) {
+    if ( (evt.keyCode || evt.which) === 27 ) {
+      $('.dropdown_select').removeClass('open');
     }
-    function wrapElement(el, wrapper, i, placeholder) {
-      el.parentNode.insertBefore(wrapper, el);
-      wrapper.appendChild(el);
-
-      document.addEventListener('click', function (e) {
-        let clickInside = wrapper.contains(e.target);
-        if (!clickInside) {
-          let menu = wrapper.getElementsByClassName('select-dropdown__list');
-          menu[0].classList.remove('active');
-        }
-      });
-
-      var buttonElement = document.createElement("button"),
-        spanElement = document.createElement("span"),
-        spanText = document.createTextNode(placeholder);
-        iElement = document.createElement("i");
-        ulElement = document.createElement("ul");
-
-      wrapper.className = 'select-dropdown select-dropdown--' + i;
-      buttonElement.className = 'select-dropdown__button select-dropdown__button--' + i;
-      buttonElement.setAttribute('data-value', '');
-      buttonElement.setAttribute('type', 'button');
-      spanElement.className = 'select-dropdown select-dropdown--' + i;
-      iElement.className = 'zmdi zmdi-chevron-down';
-      ulElement.className = 'select-dropdown__list select-dropdown__list--' + i;
-      ulElement.id = 'select-dropdown__list-' + i;
-
-      wrapper.appendChild(buttonElement);
-      spanElement.appendChild(spanText);
-      buttonElement.appendChild(spanElement);
-      buttonElement.appendChild(iElement);
-      wrapper.appendChild(ulElement);
+  });
+  
+  $(document).on('click', function(evt) {
+    if ( $(evt.target).closest(".dropdown_select > .caption").length === 0 ) {
+      $('.dropdown_select').removeClass('open');
     }
-
-    function displyUl(element) {
-
-      if (element.tagName == 'BUTTON') {
-        selectDropdown = element.parentNode.getElementsByTagName('ul');
-        var labelWrapper = document.getElementsByClassName('js-label-wrapper');
-        for (var i = 0, len = selectDropdown.length; i < len; i++) {
-          selectDropdown[i].classList.toggle("active");
-          var parentNode = $(selectDropdown[i]).closest('.js-label-wrapper');
-          parentNode[0].classList.toggle("active");
-        }
-      } else if (element.tagName == 'LI') {
-        var selectId = element.parentNode.parentNode.getElementsByTagName('select')[0];
-        selectElement(selectId.id, element.getAttribute('data-value'));
-        elementParentSpan = element.parentNode.parentNode.getElementsByTagName('span');
-        element.parentNode.classList.toggle("active");
-        elementParentSpan[0].textContent = element.textContent;
-        elementParentSpan[0].parentNode.setAttribute('data-value', element.getAttribute('data-value'));
-      }
-
-    }
-    function selectElement(id, valueToSelect) {
-      var element = document.getElementById(id);
-      element.value = valueToSelect;
-      element.setAttribute('selected', 'selected');
-    }
-    var buttonSelect = document.getElementsByClassName('select-dropdown__button');
-    for (var i = 0, len = buttonSelect.length; i < len; i++) {
-      buttonSelect[i].addEventListener('click', function (e) {
-				e.preventDefault();
-				displyUl(this);
-			}, false);
-		}
-}
+  });
+  
+});
